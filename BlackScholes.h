fdeb83147dec2a981c65084d2c23471bb9543943
ZhouShijie123 <32040381+ZhouShijie123@users.noreply.github.com>
2018-03-31 01:33:04 -0400

    Delete BlackScholes.h

--- BlackScholes.h
+++
@@ -1,9 +0,0 @@
-#ifndef BlackScholes
-#define BlackScholes
-#include <string>
-
-using namespace std;
-double Phi(double mean, double vol, double location);
-double optionPriceBS(double s0, double strike, double rd, double rf, double mature, double vol, string optionType);
-
-#endif
