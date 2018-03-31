bc52b504d794ea6ccf316c99a751ccda13b37151
ZhouShijie123 <32040381+ZhouShijie123@users.noreply.github.com>
2018-03-31 01:46:59 -0400

    Add files via upload

---
+++ EM Algorithm/EMtry.h
@@ -0,0 +1,6 @@
+#ifndef EMTRY
+#define EMTRY
+
+void EMfixData(double **myData, int **ifExist, int nRow, int nCol, double **fixedData, double **estiCovMat, double *estiMean, string* title, string outputMeanFile, string outputCovFile);
+
+#endif
