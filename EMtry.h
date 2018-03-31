3023043c41fcccf0a5182e72c5992efcf5dbefc0
ZhouShijie123 <32040381+ZhouShijie123@users.noreply.github.com>
2018-03-31 01:33:26 -0400

    Delete EMtry.h

--- EMtry.h
+++
@@ -1,6 +0,0 @@
-#ifndef EMTRY
-#define EMTRY
-
-void EMfixData(double **myData, int **ifExist, int nRow, int nCol, double **fixedData, double **estiCovMat, double *estiMean, string* title, string outputMeanFile, string outputCovFile);
-
-#endif
